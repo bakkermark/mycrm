ee3a9a8eb4b8aadd5beb5d5c7099e9c9894e34ac
Mark Bakker <60272177+bakkermark@users.noreply.github.com>
2024-02-21 20:59:37 +0100

    Photo upload now also nicy styled.

--- src/pages/user/add/index.vue
+++ src/pages/user/add/index.vue
@@ -1,68 +1,71 @@
 <template>
-  <VForm ref="refForm" @submit.prevent="handleSubmit">
-    <VRow>
-      <VCol cols="12" md="4">
-        <AppTextField v-model="firstName" label="First Name" placeholder="Type in first name ..."
-                      :rules="[requiredValidator]"/>
-      </VCol>
-      <VCol cols="12" md="2">
-        <AppTextField v-model="infix" label="Infix" placeholder="Type in infix ... "/>
-      </VCol>
-      <VCol cols="12" md="6">
-        <AppTextField v-model="lastName" label="Last Name" placeholder="Type in last name ..."
-                      :rules="[requiredValidator]"/>
-      </VCol>
-      <VCol cols="12" md="12">
-        <AppTextField v-model="email" label="Email" placeholder="Type in email address ..."
-                      :rules="[requiredValidator, emailValidator]"/>
-      </VCol>
-      <VCol cols="12" md="6">
-        <AppTextField
-          v-model="password"
-          :append-inner-icon="show1 ? 'tabler-eye-off' : 'tabler-eye' "
-          :rules="[rules.required, rules.min]"
-          :type="show1 ? 'text' : 'password'"
-          name="password"
-          label="Password"
-          hint="At least 8 characters"
-          placeholder="············"
-          counter
-          @click:append-inner="show1 = !show1"
-        />
-      </VCol>
-      <VCol
-        cols="12"
-        sm="6"
-      >
-        <AppTextField
-          v-model="confirmPassword"
-          :rules="[rules.required, rules.min]"
-          :append-inner-icon="show2 ? 'tabler-eye-off' : 'tabler-eye' "
-          :type="show2 ? 'text' : 'password'"
-          name="ConfirmPassword"
-          placeholder="············"
-          label="Confirm password"
-          hint="At least 8 characters"
-          @click:append-inner="show2 = !show2"
-        />
-      </VCol>
-      <VCol cols="12" md="6">
-        <AppSelect v-model="selectedLicenseHolder" :items="licenseHolders" :rules="[requiredValidator]"
-                   placeholder="Select a license holder ..." label="License holder" name="selectLicenseHolder" require/>
-        
-      </VCol>
-      <VCol cols="12" md="6">
-        <AppSelect
-          v-model="selectedRole"
-          :items="roles"
-          :rules="[requiredValidator]"
-          placeholder="Select a role ..."
-          label="Role"
-          name="selectRole"
-          require
-        />
-      </VCol>
-      <VCol cols="12" md="6">
+  <VCard title="Add user">
+    <VCardText class="d-flex">
+      <VForm ref="refForm" @submit.prevent="handleSubmit">
+        <VRow>
+          <VCol cols="12" md="4">
+            <AppTextField v-model="firstName" label="First Name" placeholder="Type in first name ..."
+                          :rules="[requiredValidator]"/>
+          </VCol>
+          <VCol cols="12" md="2">
+            <AppTextField v-model="infix" label="Infix" placeholder="Type in infix ... "/>
+          </VCol>
+          <VCol cols="12" md="6">
+            <AppTextField v-model="lastName" label="Last Name" placeholder="Type in last name ..."
+                          :rules="[requiredValidator]"/>
+          </VCol>
+          <VCol cols="12" md="12">
+            <AppTextField v-model="email" label="Email" placeholder="Type in email address ..."
+                          :rules="[requiredValidator, emailValidator]"/>
+          </VCol>
+          <VCol cols="12" md="6">
+            <AppTextField
+              v-model="password"
+              :append-inner-icon="show1 ? 'tabler-eye-off' : 'tabler-eye' "
+              :rules="[rules.required, rules.min]"
+              :type="show1 ? 'text' : 'password'"
+              name="password"
+              label="Password"
+              hint="At least 8 characters"
+              placeholder="············"
+              counter
+              @click:append-inner="show1 = !show1"
+            />
+          </VCol>
+          <VCol
+            cols="12"
+            sm="6"
+          >
+            <AppTextField
+              v-model="confirmPassword"
+              :rules="[rules.required, rules.min]"
+              :append-inner-icon="show2 ? 'tabler-eye-off' : 'tabler-eye' "
+              :type="show2 ? 'text' : 'password'"
+              name="ConfirmPassword"
+              placeholder="············"
+              label="Confirm password"
+              hint="At least 8 characters"
+              @click:append-inner="show2 = !show2"
+            />
+          </VCol>
+          <VCol cols="12" md="6">
+            <AppSelect v-model="selectedLicenseHolder" :items="licenseHolders" :rules="[requiredValidator]"
+                       placeholder="Select a license holder ..." label="License holder" name="selectLicenseHolder"
+                       require/>
+
+          </VCol>
+          <VCol cols="12" md="6">
+            <AppSelect
+              v-model="selectedRole"
+              :items="roles"
+              :rules="[requiredValidator]"
+              placeholder="Select a role ..."
+              label="Role"
+              name="selectRole"
+              require
+            />
+          </VCol>
+          <VCol cols="1" md="1">
             <!-- 👉 Avatar -->
             <VAvatar
               rounded
@@ -70,99 +73,78 @@
               class="me-6"
               :image="avatarImage"
             />
-
+          </VCol>
+          <VCol cols="5" md="5">
             <!-- 👉 Upload Photo -->
             <form class="d-flex flex-column justify-center gap-4">
               <div class="d-flex flex-wrap gap-2">
-                <VBtn
-                  color="primary"
-                  @click="refInputEl?.click()"
-                >
-                  <VIcon
-                    icon="tabler-cloud-upload"
-                    class="d-sm-none"
-                  />
+                <VBtn color="primary" @click="refInputEl?.click()">
+                  <VIcon icon="tabler-cloud-upload" class="d-sm-none"/>
                   <span class="d-none d-sm-block">Upload photo</span>
                 </VBtn>
-
-                <input
-                  ref="refInputEl"
-                  type="file"
-                  name="file"
-                  accept=".jpeg,.png,.jpg,GIF"
-                  hidden
-                  @input="changeAvatar"
-                >
-
-                <VBtn
-                  type="reset"
-                  color="secondary"
-                  variant="tonal"
-                  @click="resetAvatar"
-                >
+                <input ref="refInputEl" type="file" name="file" accept=".jpeg,.png,.jpg,GIF" hidden @input="changeAvatar">
+                <VBtn type="reset" color="secondary" variant="tonal" @click="resetAvatar">
                   <span class="d-none d-sm-block">Reset</span>
-                  <VIcon
-                    icon="tabler-refresh"
-                    class="d-sm-none"
-                  />
+                  <VIcon icon="tabler-refresh" class="d-sm-none"/>
                 </VBtn>
               </div>
-
               <p class="text-body-1 mb-0">
                 Allowed JPG, GIF or PNG. Max size of 800K
               </p>
             </form>
-      </VCol>
-      <VCol cols="12">
-        <VBtn
-          type="submit"
-          class="me-2"
-        >
-          Save
-          <VTooltip
-            open-delay="500"
-            activator="parent"
-            location="top"
-          >
-            Save the user data
-          </VTooltip>
-        </VBtn>
+          </VCol>
+          <VCol cols="12">
+            <VBtn
+              type="submit"
+              class="me-2"
+            >
+              Save
+              <VTooltip
+                open-delay="500"
+                activator="parent"
+                location="top"
+              >
+                Save the user data
+              </VTooltip>
+            </VBtn>
 
-        <VBtn
-          color="secondary"
-          type="reset"
-          variant="tonal"
-        >
-          Reset
-          <VTooltip
-            open-delay="500"
-            activator="parent"
-            location="top"
-          >
-            Reset the form
-          </VTooltip>
-        </VBtn>
-      </VCol>
-    </VRow>
-  </VForm>
+            <VBtn
+              color="secondary"
+              type="reset"
+              variant="tonal"
+            >
+              Reset
+              <VTooltip
+                open-delay="500"
+                activator="parent"
+                location="top"
+              >
+                Reset the form
+              </VTooltip>
+            </VBtn>
+          </VCol>
+        </VRow>
+      </VForm>
+    </VCardText>
+  </VCard>
 </template>
 
 <script lang="ts" setup>
-import { ref as vueRef, onMounted } from 'vue';
-import { useRouter } from 'vue-router';
-import { doc, setDoc } from 'firebase/firestore';
-import { ref as fbRef, getDownloadURL, uploadBytes } from '@firebase/storage';
-import { auth, projectFirestore, projectStorage } from '@/firebase/config';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import {ref as vueRef, onMounted} from 'vue';
+import {useRouter} from 'vue-router';
+import {doc, setDoc} from 'firebase/firestore';
+import {ref as fbRef, getDownloadURL, uploadBytes} from '@firebase/storage';
+import {auth, projectFirestore, projectStorage} from '@/firebase/config';
+import {createUserWithEmailAndPassword} from 'firebase/auth';
 import AppTextField from "@core/components/app-form-elements/AppTextField.vue";
-import type { VForm } from 'vuetify/components';
+import type {VForm} from 'vuetify/components';
 import getLicenses from "@/composables/getLicenses";
-import { useI18n } from 'vue-i18n';
-import { useSnackbarStore } from "@/plugins/pinia/snackbarStore";
+import {useI18n} from 'vue-i18n';
+import {useSnackbarStore} from "@/plugins/pinia/snackbarStore";
 
-const { t } = useI18n();
+const {t} = useI18n();
 const avatarImage = vueRef('/Avatars/14.png')
-const { licenses, load } = getLicenses();
+const {licenses, load} = getLicenses();
 const routePushName = 'user-list'
 const firebaseCollectionName = 'Users'
 const router = useRouter();
@@ -189,7 +171,7 @@ const changeAvatar = (file: Event) => {
     fileReader.readAsDataURL(files[0])
     fileReader.onload = () => {
       if (typeof fileReader.result === 'string')
-        avatarImage.value = ''
+        avatarImage.value = fileReader.result
     }
   }
 }
@@ -222,7 +204,7 @@ const handleSubmit = async () => {
         const fileRef = fbRef(projectStorage, 'Avatars/' + uid + '.' + file.name.split('.').pop());
         const snapshot = await uploadBytes(fileRef, file);
         const avatar = await getDownloadURL(snapshot.ref);
-        
+
         const data = {
           id: uid,
           firstName: firstName.value,
@@ -240,12 +222,12 @@ const handleSubmit = async () => {
         }
         await setDoc(doc(projectFirestore, firebaseCollectionName, uid), data);
         const snackbarStore = useSnackbarStore();
-        const snackBarPayload = { color: "success", message: t("User has been added successfully.") }
+        const snackBarPayload = {color: "success", message: t("User has been added successfully.")}
         snackbarStore.showSnackbar(snackBarPayload)
-        await router.push({ name: routePushName });
+        await router.push({name: routePushName});
       } catch (err) {
         const snackbarStore = useSnackbarStore();
-        const snackBarPayload = { color: "error", message: t("User could not be added. Details: " + err) }
+        const snackBarPayload = {color: "error", message: t("User could not be added. Details: " + err)}
         snackbarStore.showSnackbar(snackBarPayload)
         console.error(err);
       }
